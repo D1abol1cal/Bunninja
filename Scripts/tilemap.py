f971548f7eac6baf91e947a1c4ce5c67a605cde1
Nofel <syednofeltalh2@gmail.com>
2024-02-20 14:05:45 +0500

    Tiling is now working , incorrect positional tuple to the blit function call in Tilemap.render()

--- Scripts/tilemap.py
+++ Scripts/tilemap.py
@@ -7,7 +7,7 @@ class Tilemap:
 
         for i in range(10):
             self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant':1, 'pos': (3+i, 10)}
-            self.tilemap[';10' + str(5 + i)] = {'type': 'stone', 'variant':1, 'pos': (5+i, 10)}
+            self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant':1, 'pos': (10, 5+i)}
 
     def render(self, surf):
 
@@ -16,7 +16,7 @@ class Tilemap:
 
         for loc in self.tilemap:
             tile = self.tilemap[loc]
-            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size))
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
                 
                 
 
