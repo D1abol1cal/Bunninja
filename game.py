2675c639a7f746dcc525757becfb52871998029b
Nofel <syednofeltalh2@gmail.com>
2024-02-20 16:06:43 +0500

    I can see clouds !!!!

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import sys
 from Scripts.tilemap import Tilemap
 from Scripts.entities import PhysicsEntity
 from Scripts.utils import load_image, load_images
+from Scripts.clouds import Clouds
 class Game:
     def __init__(self):
 
@@ -24,11 +25,12 @@ class Game:
             'stone': load_images('tiles/stone'),
             'large_decor': load_images('tiles/large_decor'),
             'player': load_image('entities/player.png'),
-            'background': load_image('background.png')
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds')
         }
 
         print(self.assets)
-
+        self.clouds = Clouds(self.assets['clouds'], count=16)
         self.player = PhysicsEntity(self, 'player', (50,50), (8,15))
         self.tilemap = Tilemap(self, tile_size=16)
 
@@ -41,6 +43,8 @@ class Game:
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))    
 
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
 
             self.tilemap.render(self.display, offset=render_scroll)
 
