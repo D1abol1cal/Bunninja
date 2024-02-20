955fd3abb6167ee53abeb2a3b58735b9e10639cc
Nofel <syednofeltalh2@gmail.com>
2024-02-20 15:21:47 +0500

    Jumping / terminal velocity when falling

--- Scripts/entities.py
+++ Scripts/entities.py
@@ -6,12 +6,14 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.vel = [0,0]
+        self.collisions = {'up':False, 'down':False, 'left':False, 'right':False}
         
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
     
 
     def update(self, tilemap, movement=(0,0)):
+        self.collisions = {'up':False, 'down':False, 'left':False, 'right':False}
         frame_movement = (movement[0]+self.vel[0], movement[1]+self.vel[1])
 
         self.pos[0] += frame_movement[0]
@@ -20,8 +22,10 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:
                     entity_rect.right = rect.left
+                    self.collisions['right'] = True
                 if frame_movement[0] < 0:
                     entity_rect.left = rect.right
+                    self.collisions['left'] = True
                 self.pos[0] = entity_rect.x
 
         self.pos[1] += frame_movement[1]
@@ -30,12 +34,17 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:
                     entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
                 if frame_movement[1] < 0:
                     entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
                 self.pos[1] = entity_rect.y
         
 
         self.vel[1] = min(5, self.vel[1] + 0.1)
+
+        if self.collisions['down'] or self.collisions['up']:
+            self.vel[1] = 0
 
     def render(self, surf):
         surf.blit(self.game.assets['player'], self.pos)
--- game.py
+++ game.py
@@ -43,14 +43,16 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_a:
                         self.movement[0] = True
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_d:
                         self.movement[1] = True
+                    if event.key == pygame.K_w:
+                        self.player.vel[1] = -3
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_a:
                         self.movement[0] = False
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_d:
                         self.movement[1] = False
 
             self.screen.blit(pygame.transform.scale(self.display, (640,480)), (0,0))
