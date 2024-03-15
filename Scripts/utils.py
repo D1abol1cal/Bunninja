7e46a8829136d2b0c5d91e3d0e2f5fa2a9402500
Nofel <syednofeltalh2@gmail.com>
2024-03-15 06:49:42 +0500

    finalized

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,11 +1,12 @@
-import pygame
 import os
+
+import pygame
 
 BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
-    img.set_colorkey((0,0,0))
+    img.set_colorkey((0, 0, 0))
     return img
 
 def load_images(path):
@@ -14,26 +15,24 @@ def load_images(path):
         images.append(load_image(path + '/' + img_name))
     return images
 
-
-class Animations:
+class Animation:
     def __init__(self, images, img_dur=5, loop=True):
         self.images = images
-        self.img_dur = img_dur
         self.loop = loop
-        self.frame = 0
+        self.img_duration = img_dur
         self.done = False
-
+        self.frame = 0
+    
     def copy(self):
-        return Animations(self.images, self.img_dur, self.loop)
+        return Animation(self.images, self.img_duration, self.loop)
     
     def update(self):
         if self.loop:
-            self.frame = (self.frame + 1) % (len(self.images) * self.img_dur)
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
         else:
-            self.frame = min(self.frame + 1, len(self.iamges) * self.img_dur - 1)
-            if self.frame >= self.img_dur * len(self.images) - 1:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
                 self.done = True
-
-
+    
     def img(self):
-        return self.images[int(self.frame / self.img_dur)]
+        return self.images[int(self.frame / self.img_duration)]
